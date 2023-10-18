e54e598fb81cfb4e280098e73b810a1103bcdd79
sajadabazari <32369181+sajadabazari@users.noreply.github.com>
2023-10-18 16:28:12 +0330

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 exports.connectToDB = () => {
-    return mongoose.connect("remote-asiatech.runflare.com:30200");
+    return mongoose.connect("mongodb://admin:g7w98z6mknwpjn3@remote-asiatech.runflare.com:30200/health-tkm-service");
 };
