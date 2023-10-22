529812f19fa5779f6ea8afebb0e1ee0e3deada27
sajadabazari <32369181+sajadabazari@users.noreply.github.com>
2023-10-22 11:48:48 +0330

    Delete middlewares directory

--- middlewares/checkAccess.js
+++
@@ -1,8 +0,0 @@
-exports.checkAccess = (permissionName) => {
-    return (req, res, next) => {
-        if (!req.session.user.accessibility[permissionName]) {
-            res.render('panel/notAccessible');
-        }
-        next();
-    }
-}
